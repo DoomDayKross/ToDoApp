cdba6159bc28b6082b42cbc7c2d8746c5afcb822
DoomDayKross <21110777@student.hcmute.edu.vn>
2025-04-22 22:59:10 +0700

    update

--- README.md
+++
@@ -1,5 +0,0 @@
- * Name: Võ Anh Khoa
- * ID: 21110046
- * Class: 02FIE - Tuesday Afternoon
- * Date: 14/4/2025
- * Assignment 7: To-do List app
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -10,14 +10,6 @@ import androidx.compose.ui.Modifier
 import com.example.todolist.ui.theme.TodoListTheme
 import com.example.todolist.ui.mainPage.MainPage
 
-/**
- * Name: Võ Anh Khoa
- * ID: 21110046
- * Class: 02FIE - Tuesday Afternoon
- * Date: 14/4/2025
- * Assignment 7: To-do List app
- */
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
